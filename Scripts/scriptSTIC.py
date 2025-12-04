e957315daf44de3d9da21c62998a5d295b5f1a4a
bogalho11 <bogalho11@gmail.com>
2025-12-04 15:27:19 +0000

    Notes from prof André

--- Scripts/scriptSTIC.py
+++ Scripts/scriptSTIC.py
@@ -2,12 +2,19 @@ import pandas as pd # for pandas information https://www.w3schools.com/python/pa
 from pyargon2 import hash
 
 # Read the csv
+# Think in a way to do this iterative way, due to the input of large files
+# Verificar chunks_size
 df = pd.read_csv('')
 
 # remove multiples column
+# dont do drop, just select the ones i want to work with
 df = df.drop(columns=['ts_ms', 'mac'])
 
 # Documentation for argon2 https://pypi.org/project/pyargon2/
+# nº de iterações fixo, que seja alto mas razoavel para a perfomance
+# fazer um dicionário para não repetir chamada da função
+# usar time_cost, enconding = b64, variant = i
+# o U.U vai no param password e a password dos STIC no salt
 df['username'] = hash(df['username'],"salt")
 
 print(df.head()) 
